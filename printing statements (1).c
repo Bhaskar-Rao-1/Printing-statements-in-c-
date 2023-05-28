ff0ddfb77b8e5d0a17cf89046646ee01c86041e5
Bhaskar-Rao-1 <127121617+Bhaskar-Rao-1@users.noreply.github.com>
2023-05-28 18:47:13 +0530

    Add files via upload

---
+++ printing statements (1).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    //printing statements in c language
+    //we use printf predefined function to print statements
+    printf("Hello world \n");
+    printf("I am interested in learning c language");
+    return 0;
+ }
